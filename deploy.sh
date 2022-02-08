013a6fc7b578a11431ad1c2d669938f43e9d0db7
docsify-username <temp@qq.com>
2022-02-08 12:07:36 +0800

    modified deploy.sh : add if else control

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,20 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
+# 因为网络问题，当只有 push 不成功时，传入一个参数，代表不需要重新生成静态文件
+if [ $# == 0 ]
+then
+  # 生成静态文件
+  npm run docs:build
+  # 先推送到 main 分支
+  git push -f origin main
+elif [ $# == 1 ]
+then
+  git push -f origin main
+fi
 
-# 先推送到 main 分支
-git push -f origin main
+# 前面出现错误时，后面就不会执行了，后面的 push 无论如何都是会需要执行的
+# 如果前面 push 成功，只有后面 push 失败，则传入多个参数（大于一个）
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
